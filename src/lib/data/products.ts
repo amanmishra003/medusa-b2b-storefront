9762e894d96d319c2367d615a1de30bf771b3aa5
Aman Mishra <amanmishra4191@gmail.com>
2025-03-06 11:10:13 -0800

    Update products.ts

--- src/lib/data/products.ts
+++ src/lib/data/products.ts
@@ -34,7 +34,7 @@ export const getProductsById = async ({
       },
       headers,
       next,
-      cache: "no-store",
+      cache: "force-cache",
     })
     .then(({ products }) => products)
 }
@@ -60,7 +60,7 @@ export const getProductByHandle = async (handle: string, regionId: string) => {
       },
       headers,
       next,
-      cache: "no-store",
+      cache: "force-cache",
     })
     .then(({ products }) => products[0])
 }
@@ -113,7 +113,7 @@ export const listProducts = async ({
         },
         headers,
         next,
-        cache: "no-store",
+        cache: "force-cache",
       }
     )
     .then(({ products, count }) => {
